2f9e19d8ced213f543e76d7416b04a00d7ca218f
pookzzz <wanyizhou.z@gmail.com>
2025-01-04 14:38:10 +0800

    adding consoleApp project

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1.cpp
@@ -0,0 +1,14 @@
+// Copyright WanYi Zhou. All Rights Reserved.
+
+#include <iostream>
+
+/**
+ * Prints something
+ * @return Exit status
+ */
+int main()
+{
+  
+    std::cout << "Zhou Wan Yi" << std::endl;
+    std::cout << "Zou Jun De" << std::endl;
+}
